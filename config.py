03b74a0548d332aa03b15cc1732789bd0043f217
mariamorenobaron <147000450+mariamorenobaron@users.noreply.github.com>
2025-04-19 17:46:56 +0200

    Update config.py
    add limit in periodic conditions

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ d = 1
 
 CONFIG = {
     "dimension": d,
-    "domain_lb": np.zeros(d),
-    "domain_ub": np.ones(d),
+   "domain_lb": np.zeros(d),
+    "domain_ub": np.full(d, 2*np.pi) if periodic else np.ones(d),
     "M": 100.0,
 
     "architecture": "MLP",
