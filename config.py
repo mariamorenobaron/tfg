3885fd98aaf500f273265540796469cceb87708b
mariamoreno <maria.morenob305@gmail.com>
2025-06-17 19:11:21 +0200

    1

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-d = 2
+d = 5
 
 CONFIG = {
     "dimension": d,
@@ -10,18 +10,18 @@ CONFIG = {
 
     "architecture": "MLP",
     "optimizer": "adam",
-    "method": "ipmnn",                       # "pmnn" or "ipmnn"
+    "method": "pmnn",                       # "pmnn" or "ipmnn"
     "depth": 4,
-    "width": 20,
+    "width": 40,
     "push_to_git" : True,
 
     "adam_steps": 50000,
     "adam_lr": 1e-3,
     "lbfgs_steps": 0,
-    "n_train": 20000,
+    "n_train": 50000,
     "fixed_min_loss": None,
 
-    "lambda_true": d * np.pi**2,
+    "lambda_true": 100 - d * np.pi**2,
     "exact_u": lambda x: np.prod(np.sin(np.pi * x), axis=1, keepdims=True),
 
     "periodic": False,
