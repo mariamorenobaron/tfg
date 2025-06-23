6c24cf965b43a34147bdc455d650e3e73a71a4df
mariamoreno <maria.morenob305@gmail.com>
2025-06-23 22:10:53 +0200

    1

--- config.py
+++ config.py
@@ -11,8 +11,8 @@ CONFIG = {
     "architecture": "MLP",
     "optimizer": "adam",
     "method": "pmnn",                      # "pmnn" or "ipmnn"
-    "depth": 2,
-    "width": 60,
+    "depth": 4,
+    "width": 20,
     "push_to_git" : True,
 
     "adam_steps": 50000,
