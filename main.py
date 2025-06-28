539bdc5410496e409277b0192f7b2541ddfaab98
mariamoreno <maria.morenob305@gmail.com>
2025-06-28 12:32:36 +0200

    1

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ import time
 from results import generate_plots_from_training_and_push
 
 if __name__ == "__main__":
-    generate_plots_from_training_and_push('numerical_experiments/Part1_power_method/pmnn_MLP_1D_d4_w20/',  push_to_git= True)
-    #pinn = run_experiment(CONFIG, save_dir='numerical_experiments/Part3_architectures')
-    #pinn.evaluate_and_plot()
+    #generate_plots_from_training_and_push('numerical_experiments/Part1_power_method/pmnn_MLP_1D_d4_w20/',  push_to_git= True)
+    pinn = run_experiment(CONFIG, save_dir='numerical_experiments/Part3_architectures')
+    pinn.evaluate_and_plot()
