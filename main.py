6f7b63e02908dd4ba21d6a1fbf65694de966b23e
mariamoreno <maria.morenob305@gmail.com>
2025-06-28 12:30:57 +0200

    1

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ from results import generate_plots_from_training_and_push
 
 if __name__ == "__main__":
     generate_plots_from_training_and_push('numerical_experiments/Part1_power_method/pmnn_MLP_1D_d4_w20/',  push_to_git= True)
-    #pinn = run_experiment(CONFIG, save_dir='numerical_experiments/Part3_inverse_architectures')
+    #pinn = run_experiment(CONFIG, save_dir='numerical_experiments/Part3_architectures')
     #pinn.evaluate_and_plot()
