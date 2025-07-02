5a1eb8721051ac22819020d1413aa0dcf7b7c10e
mariamoreno <maria.morenob305@gmail.com>
2025-07-02 17:02:45 +0200

    Update width parameter in config.py from 20 to 40

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ CONFIG = {
     "optimizer": "adam",
     "method": "pmnn",                      # "pmnn" or "ipmnn"
     "depth": 2,
-    "width": 40,  # probar con 60
+    "width": 80,  # probar con 60
     "push_to_git" : True,
 
     "adam_steps": 15000,  # Bajar Iteraciones para Bloque4
