f834135bb52f71eee64b13e1cb81d946a3591b85
mariamoreno <maria.morenob305@gmail.com>
2025-07-06 16:51:27 +0200

    Refactor model loading and evaluation in main.py and results.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ torch.set_default_dtype(torch.float64)
 
 if __name__ == "__main__":
     u_exact = lambda x: np.prod(np.sin(np.pi * x), axis=1, keepdims=True)
-    #generate_plots_from_training_and_push('numerical_experiments/Part1_power_method/pmnn_MLP_1D_d4_w20/',  push_to_git= True)
+    generate_plots_from_training_and_push('numerical_experiments/Part1_power_method/pmnn_MLP_2D_d4_w20/',  push_to_git= True)
     #pinn = run_model(CONFIG, save_dir='numerical_experiments/Part5_epochs_analysis')
     #pinn.evaluate_and_plot()
-    evaluate_model_and_generate_results('numerical_experiments/Part1_power_method/pmnn_MLP_1D_d4_w20/',True)
+    evaluate_model_and_generate_results('numerical_experiments/Part1_power_method/pmnn_MLP_2D_d4_w20/',True)
