f53d0b589a979203f230ab0912dabf7eb993fae1
mariamoreno <maria.morenob305@gmail.com>
2025-07-06 21:18:47 +0200

    Update plot generation path for MLP model in main.py

--- main.py
+++ main.py
@@ -6,11 +6,9 @@ import numpy as np
 torch.set_default_dtype(torch.float64)
 
 if __name__ == "__main__":
-    generate_plots_from_training_and_push('numerical_experiments/Part4_architectures/pmnn_MLP_2D_d2_w20/',  push_to_git= True)
-    generate_plots_from_training_and_push('numerical_experiments/Part4_architectures/pmnn_MLP_2D_d2_w40/',  push_to_git= True)
-    generate_plots_from_training_and_push('numerical_experiments/Part4_architectures/pmnn_MLP_2D_d2_w80/',  push_to_git= True)
+    generate_plots_from_training_and_push('numerical_experiments/Part4_architectures/pmnn_MLP_2D_d4_w20/',  push_to_git= True)
+    generate_plots_from_training_and_push('numerical_experiments/Part4_architectures/pmnn_MLP_2D_d4_w40/',  push_to_git= True)
     #pinn = run_model(CONFIG, save_dir='numerical_experiments/Part5_epochs_analysis')
     #pinn.evaluate_and_plot()
-    evaluate_model_and_generate_results('numerical_experiments/Part4_architectures/pmnn_MLP_2D_d2_w20/',True)
-    evaluate_model_and_generate_results('numerical_experiments/Part4_architectures/pmnn_MLP_2D_d2_w40/',True)
-    evaluate_model_and_generate_results('numerical_experiments/Part4_architectures/pmnn_MLP_2D_d2_w80/',True)
+    evaluate_model_and_generate_results('numerical_experiments/Part4_architectures/pmnn_MLP_2D_d4_w20/',True)
+    evaluate_model_and_generate_results('numerical_experiments/Part4_architectures/pmnn_MLP_2D_d4_w40/',True)
