df54b9a33b7a639c6729fec9779815ee5716a98d
mariamoreno <maria.morenob305@gmail.com>
2025-07-06 17:25:00 +0200

    Update plot generation path for MLP model in main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import numpy as np
 torch.set_default_dtype(torch.float64)
 
 if __name__ == "__main__":
-    generate_plots_from_training_and_push('numerical_experiments/Part2_inverse_power_method/ipmnn_MLP_1D_d4_w20/',  push_to_git= True)
+    generate_plots_from_training_and_push('numerical_experiments/Part2_inverse_power_method/ipmnn_MLP_2D_d4_w20/',  push_to_git= True)
     #pinn = run_model(CONFIG, save_dir='numerical_experiments/Part5_epochs_analysis')
     #pinn.evaluate_and_plot()
-    evaluate_model_and_generate_results('numerical_experiments/Part2_inverse_power_method/ipmnn_MLP_1D_d4_w20/',True)
+    evaluate_model_and_generate_results('numerical_experiments/Part2_inverse_power_method/ipmnn_MLP_2D_d4_w20/',True)
