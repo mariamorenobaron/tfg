c3e176deb7e2ee67da40451da6cec637fb32ca74
mariamoreno <maria.morenob305@gmail.com>
2025-07-08 12:03:00 +0200

    Refactor evaluation function calls and update results formatting

--- config.py
+++ config.py
@@ -10,22 +10,21 @@ CONFIG = {
 
     "architecture": "MLP",     # "MLP" or "ResNet"
     "optimizer": "adam",
-    "method": "ipmnn",                      # "pmnn" or "ipmnn"
+    "method": "pmnn",                      # "pmnn" or "ipmnn"
     "depth": 4,
     "width": 20,
     "push_to_git" : True,
 
-    "adam_steps": 50000,
+    "adam_steps": 5000,
     "adam_lr": 1e-3,
     "n_train": 20000,
     "early_stopping": False,
     "tolerance": 1e-6,
-    "use_seed": False,
+    "use_seed": True,
     "seed": 2,
 
-    "lambda_true": d * np.pi**2,
+    "lambda_true": 100 - d * np.pi**2,
     "exact_u": lambda x: np.prod(np.sin(np.pi * x), axis=1, keepdims=True),
-
 
     "periodic": False,
     "pbc_k": 1,
