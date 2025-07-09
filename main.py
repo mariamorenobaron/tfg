088bf747d972aa3116d330e005b0f91fff9dd55c
mariamoreno <maria.morenob305@gmail.com>
2025-07-09 18:28:54 +0200

    1

--- main.py
+++ main.py
@@ -9,5 +9,5 @@ if __name__ == "__main__":
     pinn = run_model(CONFIG, save_dir='numerical_experiments/Part5_epochs_analysis')
     #pinn = run_model(CONFIG, save_dir='numerical_experiments/Part2_inverse_power_method')
     pinn.evaluate_and_plot()
-    generate_plots_from_training_and_push('numerical_experiments/Part5_epochs_analysis/pmnn_MLP_2D_d4_w20_epochs50000/',  push_to_git= True)
-    evaluate_model_and_generate_results('numerical_experiments/Part5_epochs_analysis/pmnn_MLP_2D_d4_w20_epochs50000/',True)
+    generate_plots_from_training_and_push('numerical_experiments/Part5_epochs_analysis/pmnn_MLP_2D_d4_w20_epochs5000/',  push_to_git= True)
+    evaluate_model_and_generate_results('numerical_experiments/Part5_epochs_analysis/pmnn_MLP_2D_d4_w20_epochs5000/',True)
