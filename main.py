9be7e9e28737f53633ad488ee438f82a509706c1
mariamoreno <maria.morenob305@gmail.com>
2025-07-10 17:54:45 +0200

    1

--- main.py
+++ main.py
@@ -18,8 +18,16 @@ def load_config(config_path):
     return config_module.CONFIG
 
 if __name__ == "__main__":
-    pinn = run_model(CONFIG, save_dir='numerical_experiments/Part4_architectures')
-    generate_plots_from_training_and_push('numerical_experiments/Part4_architectures/pmnn_MLP_2D_d8_w20/', push_to_git=True)
+    #pinn = run_model(CONFIG, save_dir='numerical_experiments/Part4_architectures')
+    #generate_plots_from_training_and_push('numerical_experiments/Part4_architectures/pmnn_MLP_2D_d8_w20/', push_to_git=True)
+
+    evaluate_model_and_generate_results('numerical_experiments/Part5_epochs_analysis/pmnn_MLP_2D_d4_w20_epochs5000', 50000, push_to_git=True)
+    evaluate_model_and_generate_results('numerical_experiments/Part5_epochs_analysis/pmnn_MLP_2D_d4_w20_epochs15000', 50000, push_to_git=True)
+    evaluate_model_and_generate_results('numerical_experiments/Part5_epochs_analysis/pmnn_MLP_2D_d4_w20_epochs25000', 50000, push_to_git=True)
+    evaluate_model_and_generate_results('numerical_experiments/Part5_epochs_analysis/pmnn_MLP_2D_d4_w20_epochs50000', 50000, push_to_git=True)
+    evaluate_model_and_generate_results('numerical_experiments/Part5_epochs_analysis/pmnn_MLP_2D_d4_w20_epochs75000', 50000, push_to_git=True)
+    evaluate_model_and_generate_results('numerical_experiments/Part5_epochs_analysis/pmnn_MLP_2D_d4_w20_epochs100000', 50000, push_to_git=True)
+
 
 
     #pinn1 = run_model(load_config('numerical_experiments/Part1_power_method/pmnn_MLP_10D_d4_w80/config.py'), save_dir='numerical_experiments/Part1_power_method')
--- numerical_experiments/Part3_shifted_ipmnn/ipmnn_MLP_1D_d4_w20_alpha225/config.py
+++ numerical_experiments/Part3_shifted_ipmnn/ipmnn_MLP_1D_d4_w20_alpha225/config.py
@@ -23,8 +23,8 @@ CONFIG = {
     "use_seed": False,
     "seed": 2,
 
-    "lambda_true": (6 * np.pi)**2,
-    "exact_u": lambda x: np.prod(np.sin(6 * np.pi * x),axis=1, keepdims=True),
+    "lambda_true": (25 * np.pi)**2,
+    "exact_u": lambda x: np.prod(np.sin(25 * np.pi * x),axis=1, keepdims=True),
 
     "periodic": False,
     "pbc_k": 1,
