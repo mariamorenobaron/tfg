fee83a5989b0d9fd453c15076c774278ac806e3c
mariamoreno <maria.morenob305@gmail.com>
2025-07-10 14:42:49 +0200

    1

--- numerical_experiments/Part4_architectures/pmnn_MLP_2D_d2_w20/config.py
+++ numerical_experiments/Part4_architectures/pmnn_MLP_2D_d2_w20/config.py
@@ -12,7 +12,7 @@ CONFIG = {
     "optimizer": "adam",
     "method": "pmnn",                      # "pmnn" or "ipmnn"
     "depth": 2,
-    "width": 80,
+    "width": 20,
     "push_to_git" : True,
 
     "adam_steps": 50000,
