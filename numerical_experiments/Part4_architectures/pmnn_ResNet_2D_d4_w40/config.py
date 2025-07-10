cdff622333ac046935d99c0e254d4f75e5f45766
mariamoreno <maria.morenob305@gmail.com>
2025-07-10 23:44:07 +0200

    1

--- numerical_experiments/Part4_architectures/pmnn_ResNet_2D_d4_w40/config.py
+++ numerical_experiments/Part4_architectures/pmnn_ResNet_2D_d4_w40/config.py
@@ -12,7 +12,7 @@ CONFIG = {
     "optimizer": "adam",
     "method": "pmnn",                      # "pmnn" or "ipmnn"
     "depth": 4,                            # number of blocks in ResNet or number of hidden layers in MLP
-    "width": 40,
+    "width": 80,
     "push_to_git" : True,
 
     "adam_steps": 50000,
