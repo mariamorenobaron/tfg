ee42a2ea1050f2044e81403e74806c7db4ab8dc6
mariamoreno <maria.morenob305@gmail.com>
2025-08-18 19:06:55 +0200

    Add configuration file and update seed value in main.py

--- main.py
+++ main.py
@@ -94,7 +94,8 @@ def collect_and_summarize(base_path: str, suffix: str):
 
 if __name__ == "__main__":
 
-    collect_results('numerical_experiments/Part1_power_method/loss')
+    results = collect_results('numerical_experiments/Part1_power_method/loss')
+    print(results)
 
     #run_model_all_criteria(CONFIG, save_dir='numerical_experiments/Part1_power_method/seed100')
     #run_model_all_criteria(CONFIG, save_dir='numerical_experiments/Part1_power_method/seed200')
